88192bf85461d364b91e95537ede7b9f1e26ea66
牛加人等于朱 <niuren.zhu@icloud.com>
2017-11-03 14:07:10 +0800

    解决传入查询的使用问题

--- ibas.organizedroles.service/src/main/webapp/bsui/c/ownership/OwnershipListView.ts
+++ ibas.organizedroles.service/src/main/webapp/bsui/c/ownership/OwnershipListView.ts
@@ -268,11 +268,11 @@ export class OwnershipListView extends ibas.BOListView implements IOwnershipList
         }
         this.table.setBusy(false);
     }
-    private lastCriteria: ibas.ICriteria;
+
     /** 记录上次查询条件，表格滚动时自动触发 */
     query(criteria: ibas.ICriteria): void {
         super.query(criteria);
-        this.lastCriteria = criteria;
+
         // 清除历史数据
         if (this.isDisplayed) {
             this.table.setBusy(true);
