adc43116f8ba1d3e1064876836c5bcfdfe476e78
牛加人等于朱 <niuren.zhu@icloud.com>
2017-11-23 11:36:21 +0800

    添加文件上传下载方法

--- ibas.organizedroles.service/src/main/webapp/api/BORepository.d.ts
+++ ibas.organizedroles.service/src/main/webapp/api/BORepository.d.ts
@@ -8,13 +8,26 @@
 
 import {
     FetchCaller,
-    SaveCaller
+    SaveCaller,
+    UploadFileCaller,
+    DownloadFileCaller,
+    FileData
 } from "ibas/index";
 import * as bo from "./bo/index"
 
 /** 业务仓库 */
 export interface IBORepositoryOrganizedRoles {
 
+    /**
+     * 上传文件
+     * @param caller 调用者
+     */
+    upload(caller: UploadFileCaller<FileData>);
+    /**
+     * 下载文件
+     * @param caller 调用者
+     */
+    download(caller: DownloadFileCaller<Blob>);
     /**
      * 查询 组织-结构
      * @param fetcher 查询者
